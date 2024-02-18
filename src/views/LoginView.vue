0b15d68b6f88522e9b3931e6b9daaea7b30d0dc2
Marksiranat <siranat2546@gmail.com>
2024-02-18 09:28:38 +0700

    พัฒนาLogin

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,43 @@
 <template>
-    <div><h1>Signin</h1></div>
+  <div><h1>Signin</h1></div>
+  <br /> <br />
   <div>
-    <input type="text" />
-    <input type="password" />
-    <button>Sign in</button>
+    <center>
+      <input type="text" placeholder="Email" v-model="emaill"/>
+      <br />
+      <br />
+      <input type="password" placeholder=" Password" v-model="password"/>
+      <br />
+      <br />
+      <button type="Signin" v-on:click="click()">Sign in</button>
+      <br />
+      <br />
+    </center>
   </div>
+  <br/><br/>
+  <div><center><h3>Don’t have a account? Sign up</h3></center></div>
+
 </template>
 
-<script setup>
-
-
-
-
-
-
-
+<script >
+export default {
+  name: "Login",
+  data() {
+    return {
+      emaill: null,
+      password: null,
+    };
+  },
+  methods: {
+    click() {
+      console.warn("values : ", this.emaill, this.password);
+    },
+  },
+};
 </script>
+
+<style scoped>
+h1 {
+  color: chocolate;
+}
+</style>
