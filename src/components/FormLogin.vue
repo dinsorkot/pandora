2c7220fa1b38ebd077a037a66a6752887fde8236
Marksiranat <siranat2546@gmail.com>
2024-02-18 16:20:03 +0700

    เสร็จละ ฟอนเอ็น

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,27 +1,56 @@
 <template>
   <div>
-  
-    <input type="text" placeholder="Username"  v-model="username" />
-    <br /><br />
-    <input type="password" placeholder=" Password"  v-model="password" />
-    <br /><br />
-    <button type="Signin" v-on:click="click()">Sign in</button>
+    <div class="box">
+      <input type="text" placeholder="Username" v-model="username" required /><img
+        src="./icons/icons8-user-96.png"
+        alt=""
+        style="width: 30px; height: 30px"
+      />
+    </div>
+
+    <div class="box top">
+      <input  :type="isVisible ? 'text' : 'password'" v-model="inputvalue" placeholder="Password"/>
+      
+
+      <img
+        src="./icons/icons8-eye-100.png"
+        alt=""
+        style="width: 30px; height: 30px"
+        @click="openeye()"
+      />
+    </div>
+
+    <button
+      style="color: aliceblue; font-size: 30px"
+      type="submit"
+      @click="onSummit('hello')"
+      class="top"
+    >
+      Sign in
+    </button>
     <br /><br />
   </div>
-
-  
 </template>
 
 <script setup>
 import { ref } from "vue";
 const username = ref();
 const password = ref();
+
+const onSummit = () => {
+  alert("Login Susces !!!");
+};
+
+
+
+
+
+
+
 function click() {
   console.log("username ", username.value);
   console.log("password ", password.value);
 }
-
-
 </script>
 
 <style scoped>
@@ -33,14 +62,24 @@ input {
   border-radius: 10px;
   border: 0;
   padding: 15px;
-  
 }
 button {
   background: #ff8222;
-  width: 309px;
+  width: 340px;
   height: 50px;
   border-radius: 10px;
-  
-  
+  border: 0;
+}
+.box {
+  position: relative;
+}
+.box img {
+  position: absolute;
+  left: 290px;
+  top: 15px;
+}
+
+.top {
+  margin-top: 20px;
 }
 </style>
