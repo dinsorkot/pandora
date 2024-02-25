29c44d47bdc43faa62a08293579802a66c613e29
dinsorkot <mju372ohm@gmail.com>
2024-02-25 16:07:24 +0700

    update register

--- src/stores/createUser.js
+++ src/stores/createUser.js
@@ -3,16 +3,25 @@ import { defineStore } from 'pinia'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 import { doc, setDoc } from 'firebase/firestore'
 import { db } from '../firebase'
+import { ref } from 'vue'
 
-export const useCreateUser = defineStore('counter', () => {
+export const useCreateUser = defineStore('sign_in', () => {
     const auth = getAuth()
     const router = useRouter()
-    const createUser = (username, password, repeat_password, email) => {
-        createUserWithEmailAndPassword(auth, email, password)
+
+    const new_user = ref({
+        username:"",
+        password:"",
+        email:"",
+        faceID:"",
+    })
+
+    const createUser = () => {
+        createUserWithEmailAndPassword(auth, new_user.value.email, new_user.value.password)
             .then((userCredential) => {
                 // Signed in
                 const user = userCredential.user
-                createUsername(user.uid,user.email, password, username, repeat_password)
+                createUsername(user.uid,new_user.value.username, new_user.value.password, new_user.value.email , new_user.value.faceID)
                 // ...
                
                 alert("Success")
@@ -31,7 +40,7 @@ export const useCreateUser = defineStore('counter', () => {
         uid,
         usernamed,
         passworded,
-        repeat_password,
+        faceID,
         emailed
         
     ) => {
@@ -39,11 +48,11 @@ export const useCreateUser = defineStore('counter', () => {
         await setDoc(doc(db, 'User',uid), {
             username: usernamed,
             password: passworded,
-            repeat_password: repeat_password,
+            faceID: faceID,
             email: emailed
         })
         console.log(uid)
         
     }
-    return { createUser }
+    return { createUser , new_user}
 })
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,26 +4,22 @@
     </div>
     <div class="">
         <div class="boxcenter">
-            <input type="text" v-model="username" name="username" placeholder="Username" >
-            <img class="icons" src="../assets/icons8-person-64.png" alt="">
+            <input type="text" v-model="user.username" name="username" placeholder="Username" >
         </div>
         <div class="boxcenter">
-            <input type="password" v-model="password" name="password" placeholder="Password">
-            <img class="icons" src="../assets/icons8-key-30.png" alt="">
+            <input type="password" v-model="user.password" name="password" placeholder="Password">
         </div>
         <div class="boxcenter">
-            <input type="password" v-model="repeat_password" name="repeat_password" placeholder="Repeat password">
-            <img class="icons" src="../assets/icons8-key-30.png" alt="">
+            <input type="password" v-model="user.repeat_password" name="repeat_password" placeholder="Repeat password">
         </div>
         <div class="boxcenter">
-            <input type="email" v-model="email" name="email" placeholder="Email">
-            <img class="icons" src="../assets/icons8-email-100 (1).png" alt="">
+            <input type="email" name="email" placeholder="Email" required v-model="user.email" >
         </div>
     </div>
-    <div class="buttoncenter1" >
-        <button  class="colorbutton" 
-        @click="createUser(username,password,repeat_password,email)"
-        ><RouterLink to="/faceID">Sign up</RouterLink></button>
+    <div class="buttoncenter1">
+        <button class="colorbutton" @click="add_user">
+            <RouterLink to="/faceID">Next step</RouterLink>
+        </button>
     </div>
     <div class="lasttext">
         Already have a account?
@@ -33,87 +29,93 @@
 
 <script setup>
 import { ref } from 'vue';
-import { useCreateUser } from '../stores/createEmail'
-import router from '@/router';
+import { useCreateUser } from '@/stores/createUser'
 
-const storeC = useCreateUser();
-const { createUser } = storeC
+const sign_in = useCreateUser()
 
-const username = ref();
-const password = ref();
-const repeat_password = ref();
-const email = ref();
+const user = ref({
+    username: "",
+    password: "",
+    repeat_password:"",
+    email: "",
+})
 
-function click_Alert() {
-    console.log("usernane = ", username.value);
-    console.log("password = ", password.value);
-    console.log("repeatpassword = ", repeat_password.value);
-    console.log("email = ", email.value);
+const add_user = () => {
+    sign_in.new_user.username = user.value.username;
+    sign_in.new_user.password = user.value.password;
+    sign_in.new_user.email = user.value.email;
 }
+
 
 </script>
 
-<style> 
-.textcenter {
-    /*ตัวเเรกเลย*/
-    text-align: center;
-    margin-top: 10%;
-    margin-bottom: 5%;
-}
-.boxcenter {
-    /*ตรงfrom*/
-    display: flex;
-    padding: auto;
-    margin: auto;
-    justify-content: center;
-    position: relative;
-}
-.boxcenter ::placeholder{
-    font-size: 15px;
-    color: #979797;
-    padding-left: 5%;
-   
-}
-.boxcenter img{
-    position: absolute;
-    left: 1020px;
-    top: 25px;
-}
-.boxcenter input{
-    width: 250px;
-    height: 50px;
-    margin: 15px 0px 0px 0px;
-    padding-left: 10px;
-    border-radius: 10px;
-    background-color: #3C394C;
-    color: #ffffff;
-    border: 0;
-}
-.buttoncenter1 {
-    /*ปุ่มกด*/
-    display: flex;
-    justify-content: center;
-    margin: 2%;
-}
+<style> .textcenter {
+     /*ตัวเเรกเลย*/
+     text-align: center;
+     margin-top: 10%;
+     margin-bottom: 5%;
+ }
 
-.lasttext {
-    /*sign in*/
-    display: flex;
-    justify-content: center;
-    margin-top: 5%;
-    font-size: 20px;
-}
+ .boxcenter {
+     /*ตรงfrom*/
+     display: flex;
+     padding: auto;
+     margin: auto;
+     justify-content: center;
+     position: relative;
+ }
 
-.colorbutton{
-    width: 250px;
-    height: 50px;
-    border-radius: 10px;
-    background-color: #FF8222;
-    color: #ffffff;
-    font-size: 20px;
-}
-.icons{
-    width: 30px;
-    height: 30px;
-}
+ .boxcenter ::placeholder {
+     font-size: 15px;
+     color: #979797;
+     padding-left: 5%;
+
+ }
+
+ .boxcenter img {
+     position: absolute;
+     left: 1020px;
+     top: 25px;
+ }
+
+ .boxcenter input {
+     width: 250px;
+     height: 50px;
+     margin: 15px 0px 0px 0px;
+     padding-left: 10px;
+     border-radius: 10px;
+     background-color: #3C394C;
+     color: #ffffff;
+     border: 0;
+ }
+
+ .buttoncenter1 {
+     /*ปุ่มกด*/
+     display: flex;
+     justify-content: center;
+     margin: 2%;
+ }
+
+ .lasttext {
+     /*sign in*/
+     display: flex;
+     justify-content: center;
+     margin-top: 5%;
+     font-size: 20px;
+ }
+
+ .colorbutton {
+     width: 250px;
+     height: 50px;
+     border-radius: 10px;
+     background-color: #FF8222;
+     color: #ffffff;
+     font-size: 20px;
+ }
+
+ .icons {
+     width: 30px;
+     height: 30px;
+ }
 </style>
+@/stores/createUser
